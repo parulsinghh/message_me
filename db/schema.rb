866a6ba8fbe19e3c9e1f4d1815da14584fb77bc7
Parul Singh <parul.rhl@gmail.com>
2019-07-16 15:36:38 +0100

    Add message model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_15_151401) do
+ActiveRecord::Schema.define(version: 2019_07_16_142304) do
+
+  create_table "messages", force: :cascade do |t|
+    t.text "body"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
